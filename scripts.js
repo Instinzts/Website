f14b370ddf3b678b7406ff872c6c2f65e80b7a25
Instinzts <Vilydubon@outlook.com>
2022-02-09 01:08:30 -0500

    DarkMode

--- scripts.js
+++ scripts.js
@@ -1,8 +1,42 @@
-const switchElement = document.querySelector('.switch')
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('dark');
 
-switchElement.addEventListener('click', () => {
-  document.body.classList.toggle('dark')
-})
+const darkModeToggle = document.querySelector('.switch');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('dark');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('dark', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('dark');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('dark', null);
+}
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('dark');
+
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off
+  } else {
+    disableDarkMode();
+  }
+});
+
 const launchDate = new Date('April 28, 2022 00:00:00').getTime();
 
 const intvl = setInterval(() => {
