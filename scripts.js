180e361f027ab552ecd6181bedacf37d8b047db1
Zenith <65027812+Instinzts@users.noreply.github.com>
2022-04-18 00:11:51 -0400

    updated time

--- scripts.js
+++ scripts.js
@@ -36,30 +36,3 @@ darkModeToggle.addEventListener('click', () => {
     disableDarkMode();
   }
 });
-
-const launchDate = new Date('April 28, 2022 00:00:00').getTime();
-
-const intvl = setInterval(() => {
-  const now = new Date().getTime();
-  const distance = launchDate - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  //display time 
-  document.querySelector('.days').innerHTML = days;
-  document.querySelector('.hours').innerHTML = hours;
-  document.querySelector('.minutes').innerHTML = minutes;
-  document.querySelector('.seconds').innerHTML = seconds;
-
-  let timeup = document.querySelector('.landing')
-  let after = document.querySelector('.after')
-
-  if(distance < 0 ) {
-    clearInterval(intvl);
-    timeup.style.display = 'none';
-    after.style.display = 'block';
-  }
-}, 1000)
